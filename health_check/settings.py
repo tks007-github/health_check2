c091de827f27e86178100e515d81fb39fbf4b796
tks007-github <takeshi.n.2020@gmail.com>
2021-08-04 22:29:28 +0900

    update

--- health_check/settings.py
+++ health_check/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i306u60t3xf=l2_j@2nac35=7l8%$n*3&j32b3gwvwr!ved6)y'
+SECRET_KEY = '1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
